b1b1b5716d05a2a8d48ba8ac266c03c8794b751e
Manohar <nandurisaimanohar@gmail.com>
2024-01-01 14:12:00 +0530

    http methods

--- redux/src/components/Mock.js
+++ redux/src/components/Mock.js
@@ -1,19 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react';
+import axios from 'axios';
 
 const Mock = () => {
     const [text, setText] = useState('');
+    const [out, setOut] = useState('');
 
-    async function handleEvent(event){
-         setText(event.current.value);
-         if(text !== ''){
-            const response = await fetch(`https://demo.dataverse.org/api/search?q=${text}`)
-         }
-         const data= await response.json();
-    }
+     useEffect(()=>{
+      const ans = axios.get(`https://demo.dataverse.org/api/search?q=${text}`).
+      then((response) => setOut(response))
+     },[text])
+
   return (
     <div>
-        <input type='text' placeholder='Enter text' onChange={handleEvent}/>
+        <input type='text' placeholder='Enter text' onChange={(e)=>setText(e.target.value)}/>
+        <h1 value={out}></h1>
     </div>
   )
 }
