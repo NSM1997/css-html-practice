ac84d512f72623a8759c75192a7ea9f7e7a94a35
Manohar <nandurisaimanohar@gmail.com>
2024-01-08 03:48:31 +0530

    clone deleted

--- FullStack/REST/db.js
+++ FullStack/REST/db.js
@@ -10,3 +10,28 @@ mongoose.connect(db,{
 }).catch( err=>{
     console.log('error connecting to db: ', err)
 })
+
+//schema (class == blueprint)
+const mySchema = new mongoose.Schema({
+    name:String,
+    creator:String,
+    publishedDate:{type:Date,default:Date.now},
+    isPublished: Boolean,
+    rating: Number
+})
+//model (constructor which is used to create objects)
+const myModel = mongoose.model('myModel',mySchema)
+
+//create a document ()
+async function createCourse(){
+    //object
+       const course = new myModel({
+        name :'MERN FullStack',
+        creator:'Manu',
+        isPublished : true,
+        rating: 4.8
+    })
+    const courseCreated =await course.save();
+    console.log(courseCreated)
+}
+// createCourse()
--- bms_clone/client/src/App.js
+++ bms_clone/client/src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Router} from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import "./stylesheets/theme.css"
+import "./stylesheets/alignments.css"
+import "./stylesheets/custom.css"
+import "./stylesheets/form-elements.css"
+import "./stylesheets/sizes.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+       <Router>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/register' element={<Register />}></Route>
+       </Router>      
+      </BrowserRouter>
     </div>
   );
 }
